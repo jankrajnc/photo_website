d21ddbab5f3edbed02354f12111c31c98cbb0a1f
jankrajnc <jan.krajnc@outlook.com>
2021-08-26 08:44:14 +0200

    Added ag-grid to the project for our table needs.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,13 @@
+// Angular imports.
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
+// External libraries.
+import { AgGridModule } from 'ag-grid-angular';
+
+// Pages and components.
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from '../components/main-layout/main-layout.component';
 import { UserTableComponent } from '../pages/user-table/user-table.component';
@@ -21,6 +26,7 @@ import { PhotoTableComponent } from '../pages/photo-table/photo-table.component'
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    AgGridModule.withComponents([]),
   ],
   providers: [],
   bootstrap: [AppComponent]
