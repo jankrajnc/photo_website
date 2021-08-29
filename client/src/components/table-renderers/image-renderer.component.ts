dae51966728c193699a444715383e194e37d15e5
jankrajnc <jan.krajnc@outlook.com>
2021-08-29 16:08:55 +0200

    Added text wrapping to the image renderer.

--- client/src/components/table-renderers/image-renderer.component.ts
+++ client/src/components/table-renderers/image-renderer.component.ts
@@ -4,22 +4,12 @@ import { ICellRendererAngularComp } from 'ag-grid-angular';
 // The HTML code is written here directly, as it's so short and simple that it does not justify a separate file.
 @Component({
   selector: 'image-renderer',
-  /*template: `<img width="50" height="50" src=\"{{this.params.data.thumbnailUrl}}\" style="margin: 5px 0 5px 0; border:2px solid black" >
-  <p>{{this.params.data.title}}</p>`*/
-  /*
-  template: `<div class="card" style="width: 8rem; margin: 5px 0 5px 0;">
-  <img src=\"{{this.params.data.thumbnailUrl}}\" class="card-img-top">
-  <div class="card-img-overlay d-flex align-items-end">
-    <p class="card-text" style="background: gray; color: whitesmoke;">{{this.params.data.title}}</p>
-  </div>
-</div>`
-  */
   template: `
   <div class="d-flex justify-content-center">
-    <div class="card" style="width: 11rem; max-width: 11rem; margin: 5px 0 5px 0;overflow: hidden;">
+    <div class="card" style="width: 11rem; margin: 5px 0 5px 0;">
       <img [src]="this.params?.data?.thumbnailUrl" class="card-img-top">
       <div class="card-body">
-        <p class="card-text" style="word-wrap: break-word;">{{this.params?.data?.title}}</p>
+        <p class="card-text text-wrap">{{this.params?.data?.title}}</p>
       </div>
     </div>
   </div>`
